b812c65d5078c4e5fadbd6c50785ab670a3483c9
Florian Stober <Flo.e3@gmx.de>
2016-02-05 15:36:02 +0100

    remove plugin stub

--- src/main/java/de/janmm14/minecraftchangeskin/bukkit/Main.java
+++
@@ -1,6 +0,0 @@
-package de.janmm14.minecraftchangeskin.bukkit;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class Main extends JavaPlugin {
-}
--- src/main/java/de/janmm14/minecraftchangeskin/bungee/Main.java
+++
@@ -1,6 +0,0 @@
-package de.janmm14.minecraftchangeskin.bungee;
-
-import net.md_5.bungee.api.plugin.Plugin;
-
-public class Main extends Plugin {
-}
